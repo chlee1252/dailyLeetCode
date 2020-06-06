fc6580d0ee886dc98dae7a98fcf16b24ad17b22a
Marc Lee <chlee1252@gmail.com>
2020-06-05 23:45:42 -0700

    06/05/2020

---
+++ pickIndex.py
@@ -0,0 +1,11 @@
+import random
+import bisect
+
+class Solution:
+  def __init__(self, w: [int]):
+    for i in range(1, len(w)): w[i] += w[i-1]
+    self.w = w
+  def pickIndex(self) -> int:
+    x = random.randint(1, self.w[-1])
+    return bisect.bisect_left(self.w, x)
+
