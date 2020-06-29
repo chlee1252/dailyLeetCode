10b4016a8bccc4e7b61583eafa47308a536a7e86
Marc Lee <chlee1252@gmail.com>
2020-06-28 21:06:01 -0700

    06/28/2020

---
+++ findItinerary.py
@@ -0,0 +1,29 @@
+class Solution:
+  def findItinerary(self, tickets:[[str]]) -> [str]:
+    from collections import deque
+    graph = {}
+    for ticket in tickets:
+      graph[ticket[0]] = graph.get(ticket[0], []) + ticket[1]
+
+    for start in graph:
+      graph[start] = deque(sorted(graph[start]))
+    
+    def dfs(root, graph, path, maxLen):
+      if len(path) == maxLen:
+        return True
+        
+      for k in range(len(graph.get(root,[]))):
+        end = graph[root].popleft()
+        path.append(end)
+
+        if dfs(end, graph, path, maxLen):
+          return True
+          
+        path.pop()
+        graph[root].append(end)
+    path = ["JFK"]
+    dfs("JFK", graph, path, len(tickets) + 1)
+
+    return path
+      
+      
