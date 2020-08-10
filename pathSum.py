5e74c55448876aa73d10634e207ed42b48e33fb0
Marc Lee <chlee1252@gmail.com>
2020-08-10 23:26:23 +0900

    08/10/2020

---
+++ pathSum.py
@@ -0,0 +1,23 @@
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+class Solution:
+    def helper(self, root, target, so_far, cache):
+        if root:
+            complement = so_far + root.val - target
+            if complement in cache:
+                self.result += cache[complement]
+            cache.setdefault(so_far+root.val, 0)
+            cache[so_far+root.val] += 1
+            self.helper(root.left, target, so_far+root.val, cache)
+            self.helper(root.right, target, so_far+root.val, cache)
+            cache[so_far+root.val] -= 1
+        return
+    
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        self.result = 0
+        self.helper(root, sum, 0, {0:1})
+        return self.result
