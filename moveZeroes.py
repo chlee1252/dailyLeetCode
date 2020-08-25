7cf6f2cfdaef46a95ed6da3f72082fd2ed7f6ae0
Marc Lee <chlee1252@gmail.com>
2020-08-25 20:57:56 +0900

    08/25/2020

---
+++ moveZeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+  def moveZeroes(self, nums: [int]) -> None:
+    lastNoneZeroIndex = 0
+    for i in range(len(nums)):
+      if nums[i] != 0:
+        nums[lastNoneZeroIndex] = nums[i]
+        lastNoneZeroIndex += 1
+    
+    for i in range(lastNoneZeroIndex, len(nums)):
+      nums[lastNoneZeroIndex] = 0
+  
