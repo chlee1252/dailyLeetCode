a4535910f39b46c7421a0fbef6d9b217ac4ea0e6
Marc Lee <chlee1252@gmail.com>
2020-09-01 23:47:58 +0900

    09/01/2020

---
+++ largestTimeFromDigits.py
@@ -0,0 +1,10 @@
+from itertools import permutations 
+
+class Solution:
+    def largestTimeFromDigits(self, A: [int]) -> str:
+        out = ""
+        for P in permutations(A):
+            if P[0]*10 + P[1] <= 23 and P[2] <= 5:
+                out = max(out, str(P[0])+str(P[1]) + ":" + str(P[2])+str(P[3]))
+        
+        return out
