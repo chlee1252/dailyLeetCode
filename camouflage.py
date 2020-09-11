db8a274ff19e4b5afff1d87cff0c8634823a5101
Marc Lee <chlee1252@gmail.com>
2020-09-11 21:12:54 +0900

    09/11/2020

---
+++ camouflage.py
@@ -0,0 +1,21 @@
+from collections import defaultdict, Counter
+
+def solution(clothes):
+  # Approach 1: use defaultdict
+  answer = 1              # Set init answer value as 1
+  hash = defaultdict(int)
+
+  for cloth in clothes:
+    hash[cloth[1]] += 1
+  
+  for key, value in hash.items():
+    answer *= value + 1
+  
+  return answer-1         # Remove init answer value
+
+  # Approach 2: Use Counter and reduce(function, iterable, init), Better and clean approach
+
+  hash = Counter([kind for name, kind in clothes])
+
+  return reduce(lambda x, y: x*(y+1), hash.values(), 1) - 1
+
