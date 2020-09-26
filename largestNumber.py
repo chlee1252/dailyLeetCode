45f2deff66e3a280ad7e588cc21e4848ac97a2e0
Marc Lee <chlee1252@gmail.com>
2020-09-25 17:56:18 -1000

    09/25/2020

---
+++ largestNumber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestNumber(self, nums: [int]) -> str:
+        def cmp_func(x, y):
+            """Sorted by value of concatenated string increasingly."""
+            if x + y > y + x:
+                return 1
+            elif x == y:
+                return 0
+            else:
+                return -1
+            
+        nums = [str(num) for num in nums]
+        
+        nums.sort(key = cmp_to_key(cmp_func), reverse = True)
+        
+        return ''.join(nums).lstrip('0') or '0'
