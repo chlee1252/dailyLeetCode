78e0ee89ffeea2fd91ab1d7ed6382f1ed634f8aa
Marc Lee <chlee1252@gmail.com>
2020-10-08 19:11:44 +0900

    10/08/2020

--- book/shortestPath/floyd.py
+++ book/shortestPath/floyd.py
@@ -16,8 +16,7 @@ for a in range(1, n+1):
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    if c < graph[a][b]:
-        graph[a][b] = c
+    graph[a][b] = min(c, graph[a][b])
 
 
 # Floyd Warshall
@@ -34,3 +33,24 @@ for a in range(1, n+1):
         else:
             print(graph[a][b], end=' ')
     print()
+
+
+'''
+입력 예시
+5
+14
+1 2 2
+1 3 3
+1 4 1
+1 5 10
+2 4 2
+3 4 1
+3 5 1
+4 5 3
+3 5 10
+3 1 8
+1 4 2
+5 1 7
+3 4 2
+5 2 4
+'''
