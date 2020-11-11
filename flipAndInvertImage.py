1101cd7a94c2881f7b420e94f4925d76306a42fe
Marc Lee <chlee1252@gmail.com>
2020-11-11 11:18:40 +0900

    11/11/2020

---
+++ flipAndInvertImage.py
@@ -0,0 +1,8 @@
+from typing import *
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        for row in A:
+            for i in range((len(row) + 1) // 2):
+                row[i], row[~i] = row[~i] ^ 1, row[i] ^ 1
+        return A
