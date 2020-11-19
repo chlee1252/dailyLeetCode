ecc28db3f8e78c9bfbadf5e0b7baf9f2fe3712cc
Marc Lee <chlee1252@gmail.com>
2020-11-19 11:38:29 +0900

    11/19/2020

---
+++ mirrorReflection.py
@@ -0,0 +1,9 @@
+from fractions import gcd
+
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        g = gcd(p, q)
+        p = (p / g) % 2
+        q = (q / g) % 2
+
+        return 1 if p and q else 0 if p else 2
