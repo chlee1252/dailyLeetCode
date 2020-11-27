55da0ba9d097ebb10a74fc0d1a03b0dd105dd825
Marc Lee <chlee1252@gmail.com>
2020-11-27 14:48:12 +0900

    Fixed Time Limit

--- canPartition.py
+++ canPartition.py
@@ -2,24 +2,15 @@ from typing import List
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        s=sum(nums)
-        if s%2!=0:
-            return False
-        target=s//2
-        nums.sort()
-        n=len(nums)
-     
-        def backtrack(start,curr_sum):
-            if curr_sum==target:
-                return True
-            if curr_sum>target:
-                return False
-            prev='#'
-            for i in range(start,n-1):
-                if nums[i]!=prev:
-                    prev=nums[i]
-                    if backtrack(i+1,curr_sum+nums[i]):
-                        return True
-            return False
-        
-        return backtrack(0,0)
+            s = sum(nums)
+            if s % 2: return False
+            target = s // 2
+
+            dp = [0] * (target + 1)
+            dp[0] = 1
+
+            for current in nums:
+                for new_target in range(target, current - 1, -1):
+                    dp[new_target] = dp[new_target] or dp[new_target - current]
+
+            return dp[target]
