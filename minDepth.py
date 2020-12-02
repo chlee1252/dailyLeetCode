40b87e6af6aa3d52c51918c01a130045cadb0712
Marc Lee <chlee1252@gmail.com>
2020-12-02 15:39:25 +0900

    12/02/2020

---
+++ minDepth.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        self.min = float('inf')
+        
+        def dfs(root, depth):
+            if not root:
+                return
+            
+            if not root.left and not root.right:
+                self.min = min(self.min, depth)
+                return
+            
+            dfs(root.left, depth+1)
+            dfs(root.right, depth+1)
+            
+            return
+        
+        dfs(root, 1)
+        
+        return self.min
