eec0ea90a6bb35eb3985cf74d05212db8d675f02
Marc Lee <chlee1252@gmail.com>
2020-12-02 18:31:29 +0900

    Added Random Library 12/02/2020

--- linkedListRandomNode.py
+++ linkedListRandomNode.py
@@ -1,9 +1,11 @@
+import random
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
 
     def __init__(self, head: ListNode):
